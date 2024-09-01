df20a97718220ccc899faa710364a37c052f9ac2
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-09-01 12:01:38 -0300

    Finalização do Projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./styles.css"
+
+import Card from "./components/Card"
+
+export default function App() {
+
+  return (
+    <div className="container">
+      <h1>Cards</h1>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
